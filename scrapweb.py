b2c2ba9424a69bb634f9c3e66a9cbbe4c50b1b0e
Vivek <vkdhiman93@gmail.com>
2022-05-09 16:20:05 +0530

    list program types

---
+++ scrapweb.py
@@ -0,0 +1,22 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Create a service object
+serv_obj = Service("E:\MSM_Work\Selenium\driver\chromedriver.exe")
+
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://twin-cities.umn.edu/academics-admissions/majors-programs")
+driver.implicitly_wait(10)
+# implicit, because if website loads before wait time then move on to next line of code
+
+elements = driver.find_elements(By.CSS_SELECTOR, "a.program-title--link")
+program_types = []
+for element in elements:
+    program_types.append(element.text)
+
+print(len(program_types))
