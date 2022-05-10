9f69f6f552d1f45dcabc5e4154fc130db6843435
Vivek <vkdhiman93@gmail.com>
2022-05-10 09:46:48 +0530

    program type update

--- scrapweb.py
+++ scrapweb.py
@@ -13,11 +13,17 @@ driver = webdriver.Chrome(service=serv_obj)
 
 driver.get("https://twin-cities.umn.edu/academics-admissions/majors-programs")
 driver.implicitly_wait(10)
-# implicit, because if website loads before wait time then move on to next line of code
+# implicit, because if website loads before wait time then control moves on to the next line of code
 
 elements = driver.find_elements(By.CSS_SELECTOR, "a.program-title--link")
-program_types = []
+program_title = []
 for element in elements:
-    program_types.append(element.text)
+    program_title.append(element.text)
+
+elements = driver.find_elements(By.CLASS_NAME, "program__type")
+program_types = []
+[program_types.append(i) for i in elements]
+
 
 print(len(program_types))
+print(len(program_title))
